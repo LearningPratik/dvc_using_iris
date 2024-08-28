6e74182067b373dc632e2723c2a849c83ec0be09
LearningPratik <pratik.dr71@gmail.com>
2024-08-28 21:55:56 +0530

    added comments

--- dvc/src/evaluate.py
+++ dvc/src/evaluate.py
@@ -14,17 +14,27 @@ def evaluate(param_yaml_path, model, data, data_category, live):
         param_file = yaml.safe_load(f)
 
     target = [param_file['base']['target_col']]
+
+    # Defining X and y from the passed data parameter in the evaluate function
     X = data.drop(target, axis = 1)
     y = data[target]
-
+    
+    # getting prediction probability using predict_proba()
     predictions_by_class = model.predict_proba(X)
+
+    # predicting on test data
     y_pred = model.predict(X)
     predictions = predictions_by_class[:, 1]
 
     # Use DVClive to log
+
+    # calculating average precision score for all classes and using macro average
     avg_precision = metrics.precision_score(y, y_pred, average = 'macro')
+
+    # calculating roc_auc score for all classes and using OVR strategy One Vs Rest
     roc_auc = metrics.roc_auc_score(y, predictions_by_class, multi_class = 'ovr')
 
+    # saving the summary (precision, roc_auc) for DVC 
     if not live.summary:
         live.summary = {'avg_precision' : {}, 'roc_auc' : {}}
     live.summary['avg_precision'][data_category] = avg_precision
@@ -52,6 +62,8 @@ def evaluate(param_yaml_path, model, data, data_category, live):
     #         indent = 4,
     #     )
 
+
+    # using DVC's sklearn plot for saving confusion matrix
     live.log_sklearn_plot('confusion_matrix', y,
                            predictions_by_class.argmax(-1),
                            name = f'cm/{data_category}')
@@ -63,11 +75,13 @@ if __name__ == '__main__':
     with open(param_yaml_path) as f:
         params_yaml = yaml.safe_load(f)
     
+    # getting the data for predicting the data
     model_dir = Path(params_yaml['model_dir'])
     model_file = Path('rf_model_1.pkl')
     with open(model_dir / model_file, 'rb') as f:
         model = pickle.load(f)
-
+    
+    # Taking the preocessed train and test data for evaluating
     processed_data_dir = Path(params_yaml['process']['dir'])
     processed_train_data_path = Path(params_yaml['process']['train_file'])
     train_data_path = processed_data_dir / processed_train_data_path
@@ -78,6 +92,7 @@ if __name__ == '__main__':
     test = pd.read_csv(test_data_path)
 
     # Evaluate train and test datasets
+    # saving the data to eval directory
     EVAL_PATH = 'eval'
     live = Live((Path(EVAL_PATH) / 'live'), dvcyaml = False, )
     evaluate(param_yaml_path, model, train, 'train', live)
@@ -87,11 +102,14 @@ if __name__ == '__main__':
     # Dump feature importances image
     fig, axes = plt.subplots(dpi = 100)
     fig.subplots_adjust(bottom = 0.2, top = 0.95)
+
+    # which features are important
     importances = model.feature_importances_
     X = train.drop(columns = 'species', axis = 1)
     feature_names = [f'feature {i}' for i in range(X.shape[1])]
     forest_importances = pd.Series(importances, index = feature_names).nlargest(n = 30)
     axes.set_ylabel('Mean decrease in impurity')
     forest_importances.plot.bar(ax = axes)
-
+    
+    # saving feature imporatance image
     fig.savefig(Path(EVAL_PATH) / 'importance.png')
--- dvc/src/train.py
+++ dvc/src/train.py
@@ -7,21 +7,27 @@ import pickle
 def training(param_yaml_path):
     with open(param_yaml_path) as f:
         params_yaml = yaml.safe_load(f)
-
     
+    # take processed train data from processed directory
     processed_data_dir = Path(params_yaml['process']['dir'])
     train_file_path = Path(params_yaml['process']['train_file'])
     train_data_path = processed_data_dir / train_file_path
-
+    
+    # take processed test data from processed directory
     test_file_path = Path(params_yaml['process']['test_file'])
     test_data_path = processed_data_dir / test_file_path
-
+    
+    # setting random state for reproducibility
     random_state = params_yaml['base']['random_state']
-    target = [params_yaml['base']['target_col']]
 
+    # defining target column, took value from params yaml file
+    target = [params_yaml['base']['target_col']]
+    
+    # reading data
     train = pd.read_csv(train_data_path)
     test = pd.read_csv(test_data_path)
 
+    # defining split data for X, y
     y_train = train[target]
     y_test = test[target]
 
@@ -29,19 +35,24 @@ def training(param_yaml_path):
     X_test = test.drop(target, axis = 1)
 
     random_state = params_yaml['base']['random_state']
-    n_est = params_yaml['train']['n_est']
 
+    # number estimators parameters for Random Forest
+    n_est = params_yaml['train']['n_est']
+    
+    # defining and fitting the train data on Random Forest
     rf = RandomForestClassifier(n_estimators = n_est, random_state = random_state)
     rf.fit(X_train, y_train)
 
+    # saving the model to model directory
     model_dir = Path(params_yaml['model_dir'])
     model_dir.mkdir(parents=True, exist_ok=True)
     model_file = Path('rf_model_1.pkl')
     with open(model_dir / model_file, 'wb') as f:
+
+        # using pickle library for saving the model
         pickle.dump(rf, f)
 
 
 if __name__ == '__main__':
     param_yaml_path = 'D:/Projects/DVC_pipeline/params.yaml'
     training(param_yaml_path = param_yaml_path)
-
